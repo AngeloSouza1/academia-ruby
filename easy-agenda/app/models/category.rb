0704c867b23c941039734ddb68732deaa2de34a8
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-05-29 19:59:10 -0300

    feat: criação menu CategoriasR

--- easy-agenda/app/models/category.rb
+++ easy-agenda/app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
-  #has_one :event
-  has_many :events
+    has_many :events
+
+    validates :identifier, presence: true
+    validates :name, presence: true
+
 end
