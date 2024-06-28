9323530c29c54b171dd6de31f44be964f449ca9d
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-06-28 17:01:59 -0300

    feat: criando schedules

--- easy-tools/db/schema.rb
+++ easy-tools/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_28_115607) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_28_184716) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_28_115607) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "schedules", force: :cascade do |t|
+    t.bigint "equipament_id", null: false
+    t.string "status", default: "pending"
+    t.date "period_start", null: false
+    t.date "period_end"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["equipament_id"], name: "index_schedules_on_equipament_id"
+    t.index ["status"], name: "index_schedules_on_status"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -84,4 +95,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_28_115607) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "schedules", "equipaments"
 end
